45a96d93f7a1be8e247a507f81061b5052489c6f
Lucas Robiati <lucasbrdv@gmail.com>
2025-05-13 18:16:59 -0300

    preparando para subir aws

--- frontend/src/EmailForm.js
+++ frontend/src/EmailForm.js
@@ -16,7 +16,7 @@ export default function EmailForm() {
     e.preventDefault();
     setStatus("Enviando...");
     try {
-      const res = await fetch("/send", {
+      const res = await fetch(process.env.API_URL, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ from ,to, subject, message }),
