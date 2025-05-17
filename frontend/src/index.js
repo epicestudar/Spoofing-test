778f2895385ed57f1339dafc38996ea60b1d16a2
Lucas Robiati <lucasbrdv@gmail.com>
2025-05-16 23:55:11 -0300

    add cognito

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,24 @@
+// index.js
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css"; // Estilos globais, se houver
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import { AuthProvider } from "react-oidc-context";
+
+const cognitoAuthConfig = {
+  authority: "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_6qqfkL2Rm",
+  client_id: "3cbq9ddkoo315uitikln08qji3",
+  redirect_uri: "http://localhost:3000/",
+  response_type: "code",
+  scope: "email openid",
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// wrap the application with AuthProvider
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider {...cognitoAuthConfig}>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
-
-reportWebVitals();
