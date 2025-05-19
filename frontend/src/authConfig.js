956f6e0bbd9f819636f40a07f2f0a88d9125092d
Lucas Robiati <lucasbrdv@gmail.com>
2025-05-19 19:37:01 -0300

    add cognito + PIN

---
+++ frontend/src/authConfig.js
@@ -0,0 +1,14 @@
+  
+  const cognitoAuthConfig = {
+  authority: "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_6qqfkL2Rm",
+  client_id: "3cbq9ddkoo315uitikln08qji3",
+  redirect_uri: "http://localhost:3000/",
+  response_type: "code",
+  scope: "email openid",
+};
+
+export default cognitoAuthConfig;
+export const logoutUri = "http://localhost:3000/";
+export const clientId = "3cbq9ddkoo315uitikln08qji3";
+export const cognitoDomain = "https://us-east-16qqfkl2rm.auth.us-east-1.amazoncognito.com";
+
